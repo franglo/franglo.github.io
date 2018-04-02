33263b7e485d45ec7b79c4896e4645c14f867e91
Samuel Molinari <samuel@molinari.me>
2018-04-02 10:56:01 +0100

    Use HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
@@ -13,13 +13,13 @@ import Nav from './components/Nav';
 let store = createStore(reducers);
 
 ReactDOM.render((
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <div>
         <Route exact path="/aurora/privacy-policy" component={PrivacyPolicy} />
         <Route exact path="/" component={Home} />
       </div>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
